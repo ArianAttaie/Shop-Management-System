d83d2c8ac469b0d3bf3de388f8a68c07694c2edf
arian <attaiearian@gmail.com>
2024-04-11 17:54:05 +0330

    Made a fake database in DataTier

---
+++ Data/FakeDatabase/FakeDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.FakeDatabase
+{
+    public class FakeDbContext
+    {
+        public static List<People> people = new List<People>();
+
+        public static List<Product> products = new List<Product>();
+
+        public static List<Order> orders = new List<Order>();
+
+        public static List<Invoice> invoices = new List<Invoice>();
+    }
+}
